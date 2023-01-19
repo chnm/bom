0f1e415c7cd773798df626e499204d3399d66505
Jason Heppler <jason.heppler@gmail.com>
2023-01-19 13:38:03 -0600

    Repo restructuring

---
+++ bom-processing/db/migrations/initialize.up.sql
@@ -0,0 +1,88 @@
+-- Table definitions for setting up the various elements of the data.
+-- 1. Years
+-- 2. Weeks
+-- 3. Parishes
+-- 4. Christenings
+-- 5. Causes of death
+-- 6. Bills of Mortality
+
+-- Table Definition: Years ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.year (
+    year text PRIMARY KEY,
+    id integer NOT NULL,
+);
+
+-- Table Definition: Weeks ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.week (
+    id integer NOT NULL,
+    week_id text PRIMARY KEY,
+    start_day integer,
+    start_month text,
+    end_day integer,
+    end_month text,
+    year text REFERENCES bom.year(year),
+    week_no integer,
+    split_year text
+);
+
+-- Table Definition: Parishes ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.parishes (
+    id integer PRIMARY KEY,
+    parish_name text NOT NULL UNIQUE,
+    canonical_name text NOT NULL
+);
+
+-- Table Definition: Parish Collectives ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.parish_collective (
+    id integer PRIMARY KEY,
+    collective_id text NOT NULL
+);
+
+-- Table Definition: Christenings ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.christenings (
+    id integer PRIMARY KEY,
+    christening text NOT NULL,
+    count integer,
+    week_number text,
+    start_month text,
+    end_month text,
+    year text REFERENCES bom.year(year),
+    start_day integer,
+    end_day integer
+);
+
+-- Table Definition: Christenings locations ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.christening_locations (
+    id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name text NOT NULL
+);
+
+-- Table Definition: Causes of Death ----------------------------------------------
+
+CREATE TABLE IF NOT EXISTS bom.causes_of_death (
+    death text,
+    count integer,
+    year text REFERENCES bom.year(year),
+    week_id text REFERENCES bom.week(week_id),
+    id integer NOT NULL,
+    death_id integer
+);
+
+-- Table Definition: Bills of Mortality --------------------------
+
+CREATE TABLE IF NOT EXISTS bom.bill_of_mortality (
+    id integer PRIMARY KEY,
+    parish_id integer NOT NULL REFERENCES bom.parishes(id),
+    collective_id integer REFERENCES bom.parish_collective(id),
+    count_type text NOT NULL,
+    count integer,
+    year_id text NOT NULL REFERENCES bom.year(year),
+    week_id text NOT NULL REFERENCES bom.week(week_id),
+    bill_type text
+);
