14252b16f281a6dea77fe4ce061ec776c7383063
Jason Heppler <jason.heppler@gmail.com>
2023-02-27 14:49:35 -0600

    Nuxt meta files update

--- bom-db/.nuxt/router.js
+++ bom-db/.nuxt/router.js
@@ -4,11 +4,6 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
-const _1460bb4e = () => interopDefault(import('../pages/About.vue' /* webpackChunkName: "pages/About" */))
-const _43873c0e = () => interopDefault(import('../pages/Data.vue' /* webpackChunkName: "pages/Data" */))
-const _756ffd41 = () => interopDefault(import('../pages/Team.vue' /* webpackChunkName: "pages/Team" */))
-const _d6129a9a = () => interopDefault(import('../pages/Visualizations.vue' /* webpackChunkName: "pages/Visualizations" */))
-const _136aae17 = () => interopDefault(import('../pages/WeeklyBills.vue' /* webpackChunkName: "pages/WeeklyBills" */))
 const _d3545854 = () => interopDefault(import('../pages/visualizations/causes.vue' /* webpackChunkName: "pages/visualizations/causes" */))
 const _41b16044 = () => interopDefault(import('../pages/visualizations/plague.vue' /* webpackChunkName: "pages/visualizations/plague" */))
 const _c3d54584 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
@@ -25,26 +20,6 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
-    path: "/About",
-    component: _1460bb4e,
-    name: "About"
-  }, {
-    path: "/Data",
-    component: _43873c0e,
-    name: "Data"
-  }, {
-    path: "/Team",
-    component: _756ffd41,
-    name: "Team"
-  }, {
-    path: "/Visualizations",
-    component: _d6129a9a,
-    name: "Visualizations"
-  }, {
-    path: "/WeeklyBills",
-    component: _136aae17,
-    name: "WeeklyBills"
-  }, {
     path: "/visualizations/causes",
     component: _d3545854,
     name: "visualizations-causes"
