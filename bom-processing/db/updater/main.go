8f102bd193d551b180a9db3477a0aa4e7b064f17
Jason Heppler <jason.heppler@gmail.com>
2023-03-08 10:57:42 -0600

    Updated to new schema

--- bom-processing/db/updater/main.go
+++ bom-processing/db/updater/main.go
@@ -63,6 +63,9 @@ func main() {
 
 	// 1. Unique years -------------------------------------------------------------
 	// Copy the data from the CSV file into the database.
+	// Structure of bom.year table:
+	// 	id integer GENERATED ALWAYS AS IDENTITY
+	// 	year text PRIMARY KEY,
 	yearsQuery := `
 		INSERT INTO bom.year (year)
 		VALUES ($1)
@@ -87,17 +90,27 @@ func main() {
 
 	// 2. Unique weeks -------------------------------------------------------------
 	// Insert the unique weeks into the bom.weeks table. On conflict, do nothing.
+	// bom.week table structure:
+	// 	id integer GENERATED ALWAYS AS IDENTITY,
+	// 	start_day integer,
+	// 	start_month text,
+	// 	end_day integer,
+	// 	end_month text,
+	// 	year text REFERENCES bom.year(year),
+	// 	week_no integer,
+	// 	split_year text,
+	// 	joinid text PRIMARY KEY
 	weeksQuery := `
-		INSERT INTO bom.week (year, week_no, start_day, start_month, end_day, end_month, week_id, split_year)
+		INSERT INTO bom.week (start_day, end_day, start_month, end_month, year, week_no, split_year, joinid)
 		VALUES (
-			$1,
-			CASE WHEN $2 = '' THEN NULL ELSE $2::int END,
-			CASE WHEN $3 = '' THEN NULL ELSE $3::int END,
-			$4,
-			CASE WHEN $5 = '' THEN NULL ELSE $5::int END,
-			$6,
-			$7,
-			$8
+			$1, -- start_day
+			$2, -- end_day
+			$3, -- start_month
+			$4, -- end_month
+			$5, -- year
+			$6, -- week_no
+			$7, -- split_year
+			$8 -- joinid
 		)
 		ON CONFLICT DO NOTHING
 	`
@@ -107,31 +120,42 @@ func main() {
 	// year_range [7] column.
 	log.Info().Msg("Inserting unique weeks...")
 	for _, weekRecord := range weekRecords {
-		year := weekRecord[0]
-		weekNum := weekRecord[1]
-		startDay := weekRecord[2]
-		endDay := weekRecord[3]
-		startMonth := weekRecord[4]
-		endMonth := weekRecord[6]
-		weekID := weekRecord[8]
-		splitYear := weekRecord[10]
+		if len(weekRecord) == 11 {
+			startDay := weekRecord[2]
+			endDay := weekRecord[3]
+			startMonth := weekRecord[4]
+			endMonth := weekRecord[5]
+			year := weekRecord[0]
+			weekNo := weekRecord[1]
+			splitYear := weekRecord[9]
+			joinID := weekRecord[10]
 
-		// Execute the query
-		_, err = conn.Exec(context.Background(), weeksQuery, year, weekNum, startDay, startMonth, endDay, endMonth, weekID, splitYear)
-		if err != nil {
-			log.Error().Stack().Err(err).Msg("Unable to insert unique weeks data.")
-			return
+			// Execute the query
+			_, err = conn.Exec(context.Background(), weeksQuery, year, weekNo, startDay, startMonth, endDay, endMonth, joinID, splitYear)
+			if err != nil {
+				log.Error().Stack().Err(err).Msg("Unable to insert unique weeks data.")
+				return
+			}
+			if err == nil {
+				log.Info().Msg("Sucessfully inserted unique weeks data.")
+			}
+		} else {
+			log.Error().Msg("Week record does not have 11 columns.")
 		}
-		if err == nil {
-			log.Info().Msg("Sucessfully inserted unique weeks data.")
-		}
+	}
+	if err == nil {
+		log.Info().Msg("Sucessfully inserted unique weeks data.")
 	}
 
 	// 3. Unique parish names -------------------------------------------------------
 	// Insert the parishRecords into the bom.parishes table. On conflict, do nothing.
+	// bom.parishes table structure:
+	// 	id integer PRIMARY KEY,
+	// 	parish_name text NOT NULL UNIQUE,
+	// 	canonical_name text NOT NULL
 	parishNamesQuery := `
-		INSERT INTO bom.parishes (parish_name, canonical_name) 
-		VALUES ($1, $2)
+		INSERT INTO bom.parishes (parish_name, canonical_name, id) 
+		VALUES ($1, $2, $3)
 		ON CONFLICT DO NOTHING
 	`
 
@@ -140,9 +164,10 @@ func main() {
 	for _, parishRecord := range parishRecords {
 		parishName := parishRecord[0]
 		canonicalName := parishRecord[1]
+		parishID := parishRecord[2]
 
 		// Execute the query
-		_, err = conn.Exec(context.Background(), parishNamesQuery, parishName, canonicalName)
+		_, err = conn.Exec(context.Background(), parishNamesQuery, parishName, canonicalName, parishID)
 		if err != nil {
 			log.Error().Stack().Err(err).Msg("Unable to insert parish names.")
 			return
@@ -154,26 +179,35 @@ func main() {
 
 	// 4. Causes of death ---------------------------------------------------------
 	// Insert the causes of death into the bom.causes table. On conflict, do nothing.
+	// bom.causes table structure:
+	// 	id integer GENERATED ALWAYS AS IDENTITY,
+	// 	death text,
+	// 	count integer,
+	// 	year text REFERENCES bom.year(year),
+	// 	week_id text NOT NULL REFERENCES bom.week(joinid),
+	// 	descriptive_text text
 	causesQuery := `
-		INSERT INTO bom.causes_of_death (death, count, descriptive_text, week_id)
+		INSERT INTO bom.causes_of_death (death, count, year, week_id descriptive_text)
 		VALUES (
 			$1, 
 			CASE WHEN $2 = '' THEN NULL ELSE $2::int END,
 			$3, 
-			$4
+			$4,
+			$5
 		)
 		ON CONFLICT DO NOTHING
 	`
 
 	// Assign variables to the values in the CSV file
 	for _, causeRecord := range causesRecords {
-		death := causeRecord[1]
-		count := causeRecord[2]
-		weekID := causeRecord[4]
-		description := causeRecord[7]
+		death := causeRecord[7]
+		count := causeRecord[8]
+		year := causeRecord[0]
+		description := causeRecord[9]
+		weekID := causeRecord[10]
 
 		// Execute the query
-		_, err = conn.Exec(context.Background(), causesQuery, death, count, weekID, description)
+		_, err = conn.Exec(context.Background(), causesQuery, death, count, year, description, weekID)
 		if err != nil {
 			log.Error().Stack().Err(err).Msg("Unable to insert causes of death.")
 			return
@@ -185,32 +219,40 @@ func main() {
 
 	// 5. All bills ----------------------------------------------------------------
 	// Insert the all bills into the bom.bills table. On conflict, do nothing.
+	// bom.bills table structure:
+	// 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	// 	parish_id integer NOT NULL REFERENCES bom.parishes(id),
+	// 	collective_id integer REFERENCES bom.parish_collective(id),
+	// 	count_type text NOT NULL,
+	// 	count integer,
+	// 	year_id text NOT NULL REFERENCES bom.year(year),
+	// 	week_id text NOT NULL REFERENCES bom.week(joinid),
+	// 	bill_type text
 	billsQuery := `
-		INSERT INTO bom.bill_of_mortality (id, parish_id, collective_id, count_type, count, week_id, bill_type)
+		INSERT INTO bom.bills (parish_id, collective_id, count_type, count, year_id, week_id, bill_type)
 		VALUES (
-			$1, 
-			$2, 
-			$3, 
-			$4, 
-			CASE WHEN $5 = '' THEN NULL ELSE $5::int END,
-			$6, 
-			$7
+			$1,
+			$2,
+			$3,
+			CASE WHEN $4 = '' THEN NULL ELSE $4::int END,
+			$5,
+			$6
 		)
 		ON CONFLICT DO NOTHING
 	`
 
 	// Assign variables to the values in the CSV file
 	for _, billRecord := range allBills {
-		countType := billRecord[0]
-		count := billRecord[1]
-		billType := billRecord[2]
-		parishID := billRecord[3]
-		weekID := billRecord[4]
-		collectiveID := 0 // make sure this exists in parish_collective table
-		id := billRecord[8]
+		parishID := billRecord[4]
+		collectiveID := 0
+		countType := billRecord[1]
+		count := billRecord[2]
+		yearID := billRecord[5]
+		weekID := billRecord[9]
+		billType := billRecord[3]
 
 		// Execute the query
-		_, err = conn.Exec(context.Background(), billsQuery, id, parishID, collectiveID, countType, count, weekID, billType)
+		_, err = conn.Exec(context.Background(), billsQuery, parishID, collectiveID, countType, count, yearID, weekID, billType)
 		if err != nil {
 			log.Error().Stack().Err(err).Msg("Unable to insert all bills.")
 			return
@@ -222,7 +264,16 @@ func main() {
 
 	// 6. Christenings -------------------------------------------------------------
 	// Insert the christenings into the bom.christenings table. On conflict, do nothing.
-	// year, week_number, unique_identifier, start_day, start_month, end_day, end_month, christening, count, id
+	// bom.christenings table structure:
+	// 	id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	// 	christening text NOT NULL,
+	// 	count integer,
+	// 	week_number text,
+	// 	start_month text,
+	// 	end_month text,
+	// 	year text REFERENCES bom.year(year),
+	// 	start_day integer,
+	// 	end_day integer
 	christeningsQuery := `
 		INSERT INTO bom.christenings (year, week_number, start_day, start_month, end_day, end_month, christening, count, id)
 		VALUES (
