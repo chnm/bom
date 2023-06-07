47db107e182dc7d69bb342b346f5494ce6d87906
Jason Heppler <jason.heppler@gmail.com>
2023-06-07 13:11:05 -0500

    Added causes of death visualization

--- bom-website/assets/visualizations/plague-deaths/main.js
+++ bom-website/assets/visualizations/plague-deaths/main.js
@@ -1,28 +1,23 @@
-import * as d3 from 'd3';
-import DeathsChart from './deaths-bar-chart';
+import * as d3 from "d3";
+import DeathsChart from "visualizations/plague-deaths/deaths-bar-chart";
 
-// Load the data
+// Load data
 const urls = [
-  'https://data.chnm.org/catholic-dioceses/',
-  'https://data.chnm.org/catholic-dioceses/per-decade/',
-  'https://data.chnm.org/ne/globe?location=North+America',
+  "https://data.chnm.org/bom/causes?start-year=1648&end-year=1754&limit=9000",
 ];
 const promises = [];
 urls.forEach((url) => promises.push(d3.json(url)));
 
-// Once all the data is loaded, initialize and render the visualizations
-Promise.all(promises)
+// Once the data is loaded, initialize the visualization.
+Promise.all(urls.map((url) => d3.json(url)))
   .then((data) => {
-    const deathsChart = new DeathsChart(
-      '#chrono-map',
-      { dioceses: data[0], northamerica: data[2] },
-      { width: 1000, height: 525 },
+    const deathschart = new DeathsChart(
+      "#chart",
+      { causes: data[0] },
+      { width: 960, height: 800 }
     );
-    deathsChart.render();
-
-    // Listen for changes to the slider
-    d3.select('#year').on('input', function updateViz() {
-      const year = this.valueAsNumber;
-      deathsChart.update(year);
-    });
+    deathschart.render();
+  })
+  .catch((error) => {
+    console.error("There was an error fetching the data.", error);
   });
--- bom-website/content/visualizations/counts/index.md
+++ bom-website/content/visualizations/counts/index.md
@@ -8,7 +8,7 @@ styles: visualizations/counts/style.css
 layout: visualizations
 thumbnail: plaguechart.png
 thumbdesc: "A screenshot showing a bar chart of transcribed bills counts."
-author: "Jason A. Heppler"
+author:
 ---
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Aliquet enim tortor at auctor urna nunc id cursus. Nunc mi ipsum faucibus vitae. Tortor id aliquet lectus proin nibh. Tincidunt eget nullam non nisi est sit amet facilisis. Varius quam quisque id diam vel quam elementum pulvinar. Et netus et malesuada fames ac turpis egestas maecenas. A arcu cursus vitae congue mauris rhoncus aenean vel elit. Pharetra massa massa ultricies mi quis. Faucibus pulvinar elementum integer enim. Pellentesque eu tincidunt tortor aliquam nulla. Adipiscing elit pellentesque habitant morbi tristique senectus et. Lorem ipsum dolor sit amet consectetur adipiscing.
