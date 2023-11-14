f5911978c925d9d23efeae8772b388e39c223084
Jason Heppler <jason.heppler@gmail.com>
2023-11-14 15:17:29 -0600

    Integrated new database GUI with Alpine

---
+++ bom-website/assets/js/database.js
@@ -0,0 +1,225 @@
+document.addEventListener("alpine:init", () => {
+    Alpine.data("billsData", () => ({
+      bills: [],
+      parishes: null,
+      sort: false,
+      modalOpen: false,
+      instructionsOpen: false,
+      modalBill: [],
+      disabled: false,
+      filters: {
+        selectedParishes: [],
+        selectedBillType: "Weekly",
+        selectedCountType: "",
+        selectedStartYear: 1636,
+        selectedEndYear: 1754,
+      },
+      isMissing: false,
+      isIllegible: false,
+      messages: {
+        loading: "Loading data...",
+      },
+      meta: {
+        loading: false,
+      },
+      server: {
+        limit: 25,
+        offset: 0,
+      },
+      page: 1,
+      pageSize: 25,
+      pagination: {
+        pageOffset: 25, // number of pages to show on either side of the current page
+        total: null, // total number of records from /totalbills
+      },
+      init() {
+        // Fetch the data.
+        this.fetchStaticData()
+        this.fetchData();
+      },
+      async fetchStaticData() {
+        // Data used for populating UI elements in the app.
+        // These values do not change within the app as users make choices.
+        fetch("https://data.chnm.org/bom/parishes")
+          .then((response) => response.json())
+          .then((data) => {
+            this.parishes = data;
+          })
+          .catch((error) => {
+            console.error("There was an error fetching parish data:", error);
+          });
+      },
+      async fetchData() {
+        // Data that will update with user selections.
+        this.meta.loading = true;
+
+        // 1. Bills data.
+        let response = await fetch(`https://data.chnm.org/bom/bills?start-year=${this.filters.selectedStartYear}&end-year=${this.filters.selectedEndYear}&bill-type=${this.filters.selectedBillType}&count-type=${this.filters.selectedCountType}&parish=${this.filters.selectedParishes}&limit=${this.server.limit}&offset=${this.server.offset}`)
+        let data = await response.json();
+        if (data.error) {
+          console.error("There was an error fetching bills data:", data.error);
+          this.meta.loading = false;
+          return;
+        }
+        data.forEach((d, i) => (d.id = i));
+
+        // After the data is ready, we set it to our bills object and the DOM updates
+        this.bills = data;
+        this.pagination.total = data[0].totalrecords;
+        console.log(data);
+        this.meta.loading = false;
+        this.updateUrl();
+      },
+      getTotalPages() {
+        return this.pagination.total;
+      },
+      setLimit() {
+        if (this.server.limit < 25 || this.server.limit > 100) {
+          this.server.limit = 25;
+        }
+
+        // Anytime the limit is changed, we need to keep the offset position in sync.
+        this.server.offset = (this.page - 1) * this.server.limit;
+
+        // After the new limit/offset is ready, we'll fetch the data again.
+        this.fetchData();
+      },
+      setStartYear() {
+        // store the start year in the filters object
+        this.filters.selectedStartYear = parseInt(this.filters.selectedStartYear);
+      },
+      setEndYear() {
+        // store the end year in the filters object
+        this.filters.selectedEndYear = parseInt(this.filters.selectedEndYear);
+      },
+      setCountType() {
+        // store the count type in the filters object
+        this.filters.selectedCountType = this.filters.selectedCountType;
+        if (this.filters.selectedCountType == "All") {
+          this.filters.selectedCountType = ""
+        }
+      },
+      changePage(page) {
+        if (page < 1 || page > this.pagination.lastPage) {
+          return;
+        }
+        this.page = page;
+
+        // Anytime the page is changed, we need to rerun our fetch with the appropriate
+        // offset value.
+        this.server.offset = (page - 1) * this.server.limit;
+
+        // After the new offset is ready, we'll fetch the data again.
+        this.fetchData();
+      },
+      sort(col) {
+        if (this.sortCol == col) this.sort = !this.sort;
+        this.sortCol = col;
+        this.bills.sort((a, b) => {
+          if (a[this.sortCol] < b[this.sortCol]) return this.sort ? 1 : -1;
+          if (a[this.sortCol] > b[this.sortCol]) return this.sort ? -1 : 1;
+          return 0;
+        });
+      },
+      applyFilters() {
+        // after a user has made selections, we need to get their data 
+        // and re-query the API. We get the values in the filters object
+        // and pass them to the API.
+        this.filters.selectedParishes = this.filters.selectedParishes;
+        this.filters.selectedStartYear = parseInt(this.filters.selectedStartYear);
+        this.filters.selectedEndYear = parseInt(this.filters.selectedEndYear);
+        this.filters.selectedCountType = this.filters.selectedCountType;
+
+        // we reset pagination to the first page
+        this.page = 1;
+        this.server.offset = 0;
+
+        this.fetchData();
+      },
+      updateLimitVal() {
+        // If a user changes the dropdown to a new value, we need to update the limit value.
+        this.server.limit = this.pageSize;
+      },
+      get resetFilters() {
+        // Restore defaults and query to original view
+        return () => {
+          this.filters.selectedStartYear = 1636;
+          this.filters.selectedEndYear = 1754;
+          this.filters.selectedCountType = "";
+          this.filters.selectedBillType = "Weekly"
+          this.filters.selectedParishes = [];
+          this.fetchData();
+        }
+      },
+      getCurrentPage() {
+        if (this.server.offset == 0) {
+          return 1;
+        }
+        return parseInt(
+          parseInt(this.server.offset) / parseInt(this.server.limit) + 1
+        );
+      },
+      getFirstDisplayedRow: function () {
+        return this.server.offset + 1;
+      },
+      // returns the index of last row on the page
+      getLastDisplayedRow: function () {
+        let int = parseInt(this.server.offset) + parseInt(this.server.limit);
+        if (int > this.server.total) {
+          int = this.server.total;
+        }
+        return int;
+      },
+      goToLastPage() {
+        // currently 272627 total rows
+        this.server.offset = this.getTotalPages() - this.server.limit;
+        this.fetchData();
+      },
+      getTotalRows: function () {
+        return parseInt(this.server.total);
+      },
+      getSummary: function (type = "bills", name = "rows") {
+        // if data is loading, we'll show a loading message
+        if (this.meta.loading) {
+          return this.messages.loading;
+        }
+        
+        if (type.toLowerCase() == "pages") {
+          return (
+            "Showing page <strong>" +
+            this.getCurrentPage() +
+            "</strong> of <strong>" +
+            this.getTotalPages() +
+            "</strong>"
+          );
+        }
+        if (!this.bills.length) {
+          return "No results";
+        }
+        return (
+          "Showing <strong>" +
+          this.getFirstDisplayedRow() +
+          "</strong> to <strong>" +
+          this.getLastDisplayedRow() +
+          "</strong> of <strong>" +
+          this.getTotalPages() +
+          "</strong> " +
+          name
+        );
+      },
+      updateUrl() {
+        // We want to provide URL parameters, so anytime a user updates a filter 
+        let url = new URL(window.location.href);
+        let params = new URLSearchParams(url.search.slice(1));
+        params.set("start-year", this.filters.selectedStartYear);
+        params.set("end-year", this.filters.selectedEndYear);
+        params.set("count-type", this.filters.selectedCountType);
+        params.set("parishes", this.filters.selectedParishes);
+        params.set("page", this.getCurrentPage());
+
+        // Use the history API to update the URL
+        history.pushState({}, "", `${location.pathname}?${params}`);
+
+      }
+    }));
+});
